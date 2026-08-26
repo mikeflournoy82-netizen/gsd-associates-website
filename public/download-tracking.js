5c7a73cb319600195d4e4449bbc24b3c69bf3401
Claw Assistant <mikeflournoy82@gmail.com>
2026-08-26 09:38:33 -0400

    Fix: Update download tracking to use public API endpoint instead of localhost

--- public/download-tracking.js
+++ public/download-tracking.js
@@ -27,7 +27,7 @@ window.gsdTrackResourceDownload = async function(resourceName, email, name = '')
   // 2. Send immediate email alert to Michael
   if (TRACKING_CONFIG.enable_email_alerts) {
     try {
-      const response = await fetch('http://localhost:3000/api/send-download-alert', {
+      const response = await fetch('/api/send-download-alert', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
